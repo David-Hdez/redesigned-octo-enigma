1a8ead3057960198a40fdbd08950a0e75b46c31f
David Hernandez <davidalfredoh146@gmail.com>
2024-04-13 22:04:02 -0600

    Completing a todo

--- src/components/todoapp.test.js
+++ src/components/todoapp.test.js
@@ -17,3 +17,11 @@ test('creates a todo', async () => {
 
   expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(2)
 })
+
+test('completes a todo', async () => {
+  const wrapper = mount(TodoApp)
+
+  await wrapper.get('[data-test="todo-checkbox"]').setValue(true)
+
+  expect(wrapper.get('[data-test="todo"]').classes()).toContain('completed')
+})
